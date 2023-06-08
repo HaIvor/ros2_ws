7681fbaa0d90465e9cc2af2e1196c976547572bf
haivor <halvor.b.birkeland@haugnett.no>
2023-06-08 11:08:16 +0200

    plotting stuff

--- plotting.py
+++ plotting.py
@@ -12,18 +12,19 @@ f1 = open(os.getcwd() + "/src/nodecomh" + "/log_to_plot.txt","r")
 f2 = open(os.getcwd() + "/src/nodecomx" + "/log_to_plot.txt","r")
 
 for row in f1:
-        value = row.strip()
-        if value:  # Skip empty lines
-            values1.append(float(value))
+    value = row.strip()
+    if value:  # Skip empty lines
+        values1.append(float(value))
 
 for row in f2:
-        value = row.strip()
-        if value:  # Skip empty lines
-            values2.append(float(value))
+        value2 = row.strip()
+        if value2:  # Skip empty lines
+            values2.append(float(value2))
 
-plt.plot(values1)
-plt.plot(values2)
+plt.plot(values1, "b", label="nodecomh")
+plt.plot(values2, "c", label="nodecomx")
+plt.xlabel("Sequences")
+plt.ylabel("Oxygen value")
+plt.legend()
 plt.grid()
 plt.show()
-
-print(values1)
